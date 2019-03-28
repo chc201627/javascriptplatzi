2e7707c94686a078ae5b20482fd27f0a7028a531
Camilo Hurtado <chc201627@gmail.com>
2019-03-28 17:16:55 -0500

    Operadores matematicos js

---
+++ operaciones-clase.js
@@ -0,0 +1,28 @@
+var edad=28
+// edad = edad + 1
+edad +=1
+
+var peso = 75
+// peso = peso - 5
+peso -=2
+
+//suma de numeros
+var sandwich = 1
+peso += sandwich
+
+var jugarAlFutbol = 3
+peso -= jugarAlFutbol
+
+//decimales
+var precioDevino= 200.3
+//Redondea numeros decimales
+var total = Math.round(precioDevino * 100* 3)  / 100
+// Indica cuantos decimales vamos a mostrar
+var totalStr = total.toFixed(2)
+//Convierte string a decimal
+var total2 = parseFloat(totalStr)
+
+//division
+var pizza = 8
+var persona = 2
+var cantidadDePorcionesPorPersona = pizza/persona
