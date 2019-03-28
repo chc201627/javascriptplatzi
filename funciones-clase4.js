00fc1b342ec04ab428006f65b989634719c2d2c0
Camilo Hurtado <chc201627@gmail.com>
2019-03-28 18:06:17 -0500

    Funciones

---
+++ funciones-clase4.js
@@ -0,0 +1,11 @@
+var  nombre = 'Camilo', edad = 29
+
+
+function imprimirEdad(n,e) {
+    console.log(`${n} tiene ${e} años`)
+}
+imprimirEdad(nombre,edad)
+imprimirEdad('Juan',18)
+imprimirEdad('Ismael',40)
+imprimirEdad('Sebastian',23)
+imprimirEdad(23,23)
