d4d6da8ba020dc32d4799bb1302f87c3fcafc80f
Camilo Hurtado <chc201627@gmail.com>
2019-04-02 18:44:36 -0500

    Objetos

--- clase5.js
+++ clase5.js
@@ -1,8 +1,21 @@
-var nombre = 'Camilo'
-function imprimiNombreEnMayusculas(nombre) {
-    nombre = nombre.toLocaleUpperCase()
-    console.log(nombre)
+var camilo = {
+    nombre: 'Camilo',
+    apellido: 'Hurtado',
+    edad:29
 }
 
-imprimiNombreEnMayusculas(nombre)
+var ernesto = {
+    nombre:'Ernesto',
+    apellido:'Cataño',
+    edad:26
+}
+
+function imprimiNombreEnMayusculas({nombre}) {
+    console.log(nombre.toLowerCase())
+}
+
+imprimiNombreEnMayusculas(camilo)
+imprimiNombreEnMayusculas(ernesto)
+imprimiNombreEnMayusculas({nombre: 'Pepito'})
+imprimiNombreEnMayusculas()
 
